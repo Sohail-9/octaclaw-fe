6de659b27a5b0b7fd1114edcae48c1d2cfcc5c99
sohail-9 <s.suhail9849@gmail.com>
2026-04-01 12:20:27 +0530

    refactor: remove QuickStart and CLIReference sections from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Navbar, Footer } from "@/components/layout";
 import HeroSection from "@/components/sections/Hero";
-import QuickStartSection from "@/components/sections/QuickStart";
 import ProximityFeatureSection from "@/components/sections/ProximityFeatureSection";
 import FeaturesSection from "@/components/sections/Features";
 import RoadmapSection from "@/components/sections/RoadmapSection";
 
 import HowItWorksSection from "@/components/sections/HowItWorks";
-import CLIReferenceSection from "@/components/sections/CLIReferenceSection";
 
 export default function AgentPage() {
   return (
@@ -15,12 +13,10 @@ export default function AgentPage() {
       <div className="relative pt-12">
         <HeroSection primaryHref="/waitlist" primaryLabel="Join Waitlist" secondaryHref="#proximity" secondaryLabel="See it in action" />
         <div className="pb-16">
-          <QuickStartSection />
           <ProximityFeatureSection />
           <FeaturesSection />
           <RoadmapSection />
           <HowItWorksSection />
-          <CLIReferenceSection />
         </div>
       </div>
       <Footer />
